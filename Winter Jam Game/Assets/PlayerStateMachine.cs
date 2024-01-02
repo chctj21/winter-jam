64580d71992c0ec9142fea42bf6902edbe8e7d33
Angelina010 <zhouangelina3@gmail.com>
2024-01-01 23:10:18 -0500

    commit1
    imported player assets, made scripts for finite state machine

---
+++ Winter Jam Game/Assets/PlayerStateMachine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStateMachine
+{
+    public PlayerState currentState { get; private set; }
+    public void Initialize(PlayerState _startState)
+    {
+        currentState = _startState;
+        currentState.Enter();
+    }
+
+    public void ChangeState(PlayerState _newState)
+    {
+        currentState.Exit();
+        currentState = _newState;
+        currentState.Enter();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
