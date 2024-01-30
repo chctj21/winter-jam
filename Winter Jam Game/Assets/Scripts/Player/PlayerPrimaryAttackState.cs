b02c9b4bb1cef8841244d421d4be3843ba2ce2a2
Angelina010 <zhouangelina3@gmail.com>
2024-01-29 23:37:57 -0500

    added victory screen, deleted Unity editor from namespaces
    Deleted unity editor from namespaces because game will not build otherwise, unity editor was always not being used in the scripts

--- Winter Jam Game/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
+++ Winter Jam Game/Assets/Scripts/Player/PlayerPrimaryAttackState.cs
@@ -1,8 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerPrimaryAttackState : PlayerState
 {
--- Winter Jam Game/Assets/Scripts/Player/PlayerState.cs
+++ Winter Jam Game/Assets/Scripts/Player/PlayerState.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
-using Debug = UnityEngine.Debug;
+
 public class PlayerState
 {
     protected PlayerStateMachine stateMachine;
